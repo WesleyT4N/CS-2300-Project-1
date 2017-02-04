2f02d307fa271303148ead9cc0e231ab280d8fa3
Wes T4N <wesleyt1998@gmail.com>
2017-02-04 14:34:05 -0500

    Navbar

--- project1/templates/header.php
+++ project1/templates/header.php
@@ -4,7 +4,7 @@
     <title>Wes Tan's Portfolio</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="../css/style.nested.css" />
     <link href="https://fonts.googleapis.com/css?family=Armata" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.1.1.slim.js"
     integrity="sha256-5i/mQ300M779N2OVDrl16lbohwXNUdzL/R2aVUXyXWA="
@@ -14,12 +14,10 @@
   </head>
   <body>
     <header>
-
       <nav>
-        <h1>WES TAN</h1>
-        <a href="../index.php">HOME</a>
         <a href="../services.php">SERVICES</a>
         <a href="../work.php">WORK</a>
+        <a href="../index.php"><img class="logo" src="../img/logo.png"/></a>
         <a href="../about.php">ABOUT</a>
         <a href="../contact.php">CONTACT</a>
       </nav>
